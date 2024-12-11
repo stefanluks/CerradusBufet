6177d004ff1505b7ce7e8adfb3b02b502f954741
stefanluks <stefan.luks@rede.ulbra.br>
2024-12-10 23:04:06 -0300

    adjusts: -> Hide - Galery if no itens -> Hide - Post if no itens

--- cerradusbuffet/settings.py
+++ cerradusbuffet/settings.py
@@ -63,16 +63,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'cerradusbuffet.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-# if DEBUG:
-#     DATABASES = {
-#         'default': {
-#             'ENGINE': 'django.db.backends.sqlite3',
-#             'NAME': BASE_DIR / 'db.sqlite3',
-#         }
-#     }
-# else:
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
